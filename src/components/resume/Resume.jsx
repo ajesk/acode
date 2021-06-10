b5592089cb87db810cd62d6bedf7434e1b6b3fb4
Aaron Jeskie <aaronjeskie@gmail.com>
2021-06-09 20:05:34 -0400

    chore: resume bump

--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Document, Page, pdfjs } from 'react-pdf'
-import doc from './ajeskie-resume-3-25-2021.pdf';
+import doc from './ajeskie-resume.pdf';
 import './Resume.scss';
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
