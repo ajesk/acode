b5c5b968338e33c0717a1f8fd60bbf3f8790cab4
Aaron Jeskie <aaronjeskie@gmail.com>
2021-06-09 22:17:19 -0400

    another contact change

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,9 +3,14 @@ import './Contact.scss';
 
 const Contact = () => {
   return (
-    <div className="contact">
-      If you would like to get in touch with me, feel free to refer to the contact info on my resume or LinkedIn page for my contact info. For additional info about some of the projects I have worked on refer to the GitHub link in the bottom corner of this page.
-    </div>
+    <>
+      <p className="contact">
+        If you would like to get in touch with me, feel free to refer to the contact info on my resume or LinkedIn page for my contact info.
+      </p>
+      <p className="contact">
+        For additional info about some of the projects I have worked on refer to the GitHub link in the bottom corner of this page.
+      </p>
+    </>
   );
 }
 
