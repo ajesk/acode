a796535572dba671e5f5cda7b705c0dc5ee6a139
Aaron Jeskie <aaronjeskie@gmail.com>
2021-09-20 17:21:59 -0400

    Updates

---
+++ precache-manifest.a5ab3497e154e6c0498361c49287803c.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e445daa8a13e9dd4435cbec22f1921de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8d70dd7fa6921787c1e",
+    "url": "/static/css/main.f303f0cc.chunk.css"
+  },
+  {
+    "revision": "b62b0c9f24ec8bed4b5d",
+    "url": "/static/js/2.8564c44e.chunk.js"
+  },
+  {
+    "revision": "ba4eb33a40bd36edae8bad475e6a396f",
+    "url": "/static/js/2.8564c44e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8d70dd7fa6921787c1e",
+    "url": "/static/js/main.e6eaac3d.chunk.js"
+  },
+  {
+    "revision": "b31b4d6f775e3da73740",
+    "url": "/static/js/runtime-main.64f155f8.js"
+  },
+  {
+    "revision": "1957748d5526b84ad79d766ee95ee988",
+    "url": "/static/media/ajeskie-resume.1957748d.pdf"
+  },
+  {
+    "revision": "412d16907bf8603a6de1588776ca35e9",
+    "url": "/static/media/cooking.412d1690.jpeg"
+  },
+  {
+    "revision": "eadedb1f9004609dc40d4fdbc347600e",
+    "url": "/static/media/keyboard_whitefox.eadedb1f.jpeg"
+  },
+  {
+    "revision": "c7c9b4ae1f6289d39db57acb49ff538a",
+    "url": "/static/media/keyboards_building.c7c9b4ae.jpg"
+  },
+  {
+    "revision": "3e7b87bdf1b73860c5e8a5d6230723b2",
+    "url": "/static/media/keyboards_building_2.3e7b87bd.jpg"
+  },
+  {
+    "revision": "fdd320946f2c5f73d8c1b2196bb9e352",
+    "url": "/static/media/me.fdd32094.jpeg"
+  },
+  {
+    "revision": "da31b5f101e1806230106b4177807254",
+    "url": "/static/media/pets_cats.da31b5f1.jpg"
+  },
+  {
+    "revision": "4ddde1129205fc08d30d022c68d36e17",
+    "url": "/static/media/site.4ddde112.png"
+  },
+  {
+    "revision": "db8630ebbf6c1e97dcd9250d1d082f9e",
+    "url": "/static/media/woodworking_speaker.db8630eb.jpg"
+  },
+  {
+    "revision": "4d7b5fc97cad1faa669078d9b1a95333",
+    "url": "/static/media/woodworking_speaker_in_progress.4d7b5fc9.jpg"
+  },
+  {
+    "revision": "d17567db5e8f2be50ff8d72b54f03103",
+    "url": "/static/media/woodworking_window.d17567db.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.eb604eb42e96c771005c4ca535361db4.js"
+  "/precache-manifest.a5ab3497e154e6c0498361c49287803c.js"
 );
 
 self.addEventListener('message', (event) => {
