15ba05ccf9d6b8df5c9a7b01bbedfec7904cafc5
Aaron Jeskie <aaronjeskie@gmail.com>
2023-05-18 21:42:31 -0400

    chore: removed the unused JS file and revised some of the text

--- index.js
+++
@@ -1,4 +0,0 @@
-{/* <a onclick="changePage('<object type=text/html data=about.html ></object>')">About</a> */}
-const changePage = (page) => {
-  document.getElementById("content").innerHTML = page;
-};
